dd50b52100c76a4cc2ce5408a48b74fac66b64c9
Tyler-Seliber <tylerseliber@gmail.com>
2022-03-03 13:39:37 -0500

    Add Body Parsing Middleware

--- NodeJS-Lab/express-api/app.js
+++ NodeJS-Lab/express-api/app.js
@@ -1,12 +1,19 @@
 // Require packages and set the port
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3002;
 const app = express();
 
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: true,
+ }));
+
 // Llooking for GET request at root of server (/)
 app.get('/', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.end('Hello, Server!');
+    response.send({ 
+        message: 'Node.js and Express REST API'});
 });
 
 // Start the server
